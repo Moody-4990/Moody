059143b03b4b9b111e914bb39864a9832180d4c1
Himani Raval <himaniraval@Himanis-MBP.cogeco.local>
2022-03-19 00:34:41 -0400

    Saturday March 19, 2022 -> 12:34am

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,13 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyBin6DqxbPqLkwzqyZ2zk7j8LqXP3SPsk4"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+@end
