cf0beb56bb1b26e835f43e99b921b164b9b92034
Noor-Alina <AN10_23@hotmail.com>
2022-03-24 16:26:34 -0400

    adding background color to appbar, change default

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -36,6 +36,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xffffde5a),
         title: const Text("Dashboard"),
         centerTitle: true,
       ),
